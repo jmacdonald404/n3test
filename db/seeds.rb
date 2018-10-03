a603e78cf6ceed9d147b006040ab8f2fac02e869
dextrizzle <alfonzo_01@hotmail.com>
2018-10-02 22:44:13 -0700

    add dataset

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+# csv_text = File.read(Rails.root.join('lib', 'seeds', 'data.csv'))
+csv_text = "1472709477,36216864614,0,3
+1472722196,201103786727,0,3"
+csv = CSV.parse(csv_text, :headers => [:datetime, :user, :os, :device], :encoding => 'ISO-8859-1')
+# puts csv
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  # puts Time.at(row[:datetime].to_i)
+  # puts row[:datetime]
+end
