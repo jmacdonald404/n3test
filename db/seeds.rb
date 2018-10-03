b740df774b15ca34e68bc0855551ac19ff725640
dextrizzle <alfonzo_01@hotmail.com>
2018-10-02 22:59:06 -0700

    modify seeds file for real data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,10 @@
 require 'csv'
 
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'data.csv'))
-csv_text = "1472709477,36216864614,0,3
-1472722196,201103786727,0,3"
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'data.csv'))
+# csv_text = "1472709477,36216864614,0,3
+# 1472722196,201103786727,0,3"
 csv = CSV.parse(csv_text, :headers => [:datetime, :user, :os, :device], :encoding => 'ISO-8859-1')
-# puts csv
-# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
-  # puts Time.at(row[:datetime].to_i)
-  # puts row[:datetime]
   v = Visit.new
   v.datetime = row[:datetime]
   v.user = row[:user]
