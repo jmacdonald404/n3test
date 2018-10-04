83c83e43b90fbd07cdea97b1c32c0f7d69b48d78
dextrizzle <alfonzo_01@hotmail.com>
2018-10-04 16:23:42 -0700

    prototype controller logic

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,10 +1,28 @@
 class VisitsController < ApplicationController
   def unique
-    visits = Visit.all.group(:user).count.length
-    render json: visits
+    if request.query_string.present?
+      h = Hash.new
+      h[:os] = params[:os].presence.try(:split, ",")
+      h[:device] = params[:device].presence.try(:split, ",")
+      visits = Visit.all.group(:user, :id).where(h.reject { |k, v| v.nil? }).count.length
+      render json: {"count":visits}
+    else
+      visits = Visit.all.group(:user).count.length
+      test2 = {"count":visits}
+      render json: test2
+    end
   end
   def loyal
-    visits = Visit.all.group(:user).count.length
-    render json: visits
+    if request.query_string.present?
+      h = Hash.new
+      h[:os] = params[:os].presence.try(:split, ",")
+      h[:device] = params[:device].presence.try(:split, ",")
+      visits = Visit.all.group(:user).where(h.reject { |k, v| v.nil? }).count.reject{ |k, v| v<10 }.length
+      render json: {"count":visits}
+    else
+      visits = Visit.all.group(:user).count.reject{|k,v| v<10}.length
+      test2 = {"count":visits}
+      render json: test2
+    end
   end
 end
